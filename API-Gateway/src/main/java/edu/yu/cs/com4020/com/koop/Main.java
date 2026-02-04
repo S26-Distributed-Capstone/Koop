dd86dfe9a0e0cf4c3710d4e26a889fbb25a97ef0
Michael Kupferstein <MichaelKupferstein@users.noreply.github.com>
2026-02-04 18:33:15 -0500

    added S3 skeleton routes

--- API-Gateway/src/main/java/edu/yu/cs/com4020/com/koop/Main.java
+++ API-Gateway/src/main/java/edu/yu/cs/com4020/com/koop/Main.java
@@ -4,9 +4,37 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
-            .get("/", ctx -> ctx.result("This is the first thing "))
-            .start(7070);
+        var app = Javalin.create().start(8080);
+
+        app.get("/health", ctx -> {
+            ctx.result("API Gateway is healthy!");
+        });
+
+        //S3 SKeleton Routes (place holder for now)
+
+        //GET Object
+        app.get("/{bucket}/{key}", ctx -> {
+            String bucket = ctx.pathParam("bucket");
+            String key = ctx.pathParam("key");
+            // Placeholder logic for getting an object from S3
+            ctx.result("Getting object with key: " + key + " from bucket: " + bucket);
+        });
+
+        //PUT Object
+        app.put("/{bucket}/{key}", ctx -> {
+            String bucket = ctx.pathParam("bucket");
+            String key = ctx.pathParam("key");
+            // Placeholder logic for putting an object to S3
+            ctx.result("Putting object with key: " + key + " to bucket: " + bucket);
+        });
+
+        //DELETE Object
+        app.delete("/{bucket}/{key}", ctx -> {
+            String bucket = ctx.pathParam("bucket");
+            String key = ctx.pathParam("key");
+            // Placeholder logic for deleting an object from S3
+            ctx.result("Deleting object with key: " + key + " from bucket: " + bucket);
+        });
         
     }
 }
