9edb01f08aa054229b2dac532a70648fb51258f9
Yonatan Ginsburg <yginsburg26@gmail.com>
2026-03-22 20:33:29 +0000

    remove unused import

--- common-lib/src/main/java/com/github/koop/common/pubsub/PubSub.java
+++ common-lib/src/main/java/com/github/koop/common/pubsub/PubSub.java
@@ -1,11 +1,9 @@
 package com.github.koop.common.pubsub;
 
-import java.util.function.BiConsumer;
-
 public interface PubSub extends AutoCloseable {
     void pub(String topic, byte[] message);
 
-    void sub(String topic); 
-    
+    void sub(String topic);
+
     void start(PubSubListener listener);
 }
