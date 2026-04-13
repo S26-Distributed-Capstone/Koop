24242cdb92810ce227f57ff3793b79680a60a108
Michael Kupferstein <MichaelKupferstein@users.noreply.github.com>
2026-04-13 13:16:05 -0400

    update toStorageKey method to use '/' instead of '-' for key formatting

--- query-processor/src/main/java/com/github/koop/queryprocessor/processor/StorageWorker.java
+++ query-processor/src/main/java/com/github/koop/queryprocessor/processor/StorageWorker.java
@@ -447,7 +447,7 @@ public final class StorageWorker {
     }
 
     private static String toStorageKey(String bucket, String key) {
-        return bucket + "-" + key;
+        return bucket + "/" + key;
     }
 
     private void streamReconstruct(int partition, String storageKey,
