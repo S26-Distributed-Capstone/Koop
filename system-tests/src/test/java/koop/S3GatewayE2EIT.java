8a2ab0481cdd73c6ccdcf6a4257af8a7281d5a65
Yonatan Ginsburg <yginsburg26@gmail.com>
2026-04-26 04:28:41 +0000

    start servers after metadata

--- system-tests/src/test/java/koop/S3GatewayE2EIT.java
+++ system-tests/src/test/java/koop/S3GatewayE2EIT.java
@@ -20,7 +20,6 @@ import com.github.koop.storagenode.db.Database;
 import com.github.koop.storagenode.db.RocksDbStorageStrategy;
 import io.javalin.Javalin;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Disabled;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -127,7 +126,6 @@ public class S3GatewayE2EIT {
             dataDirs.add(dir);
             databases.add(db);
 
-            server.start();
             addrs.add(new InetSocketAddress("127.0.0.1", port));
             log("[NODE " + i + "] READY on port " + port);
         }
@@ -160,7 +158,7 @@ public class S3GatewayE2EIT {
         sharedFetcher.update(esConfig);
         sharedFetcher.update(psConfig);
 
-        Thread.sleep(1000);
+        servers.forEach(StorageNodeServerV2::start);
         log("=== CLUSTER READY ===");
 
         // 5. Wire up the Gateway
